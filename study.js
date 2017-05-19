e734901a00ab983e04282634226ae48d383b6526
fujihe <fujihe219@gmail.com>
2017-05-19 11:36:09 +0900

    170519_gulp勉強

---
+++ study.js
@@ -0,0 +1,32 @@
+var navFixed = navFixed || {};
+navFixed.action = function() {
+  initialize: function() {
+    var $nav = $('.p-globalNav');
+    var $offset = $nav.offset();
+    var $is-fixed = $('.is-fixed');
+  },
+  $(window).scroll(function() {
+    if($(window).scrollTop() > $offset.top) {
+      $nav.addClass($is-fixed);
+      $nav.css('margin-left', $offset.left);
+    }else{
+      $nav.removeClass($is-fixed);
+      $nav.css('margin-left', '0');
+    }
+  });
+};
+navFixed.action();
+
+  $(function() {
+    var nav = $('.p-globalNav'),
+    offset = nav.offset();
+    $(window).scroll(function() {
+      if($(window).scrollTop() > offset.top) {
+        nav.addClass('is-fixed');
+        nav.css('margin-left', offset.left);
+      }else{
+        nav.removeClass('is-fixed');
+        nav.css('margin-left', '0');
+      }
+    });
+  });
